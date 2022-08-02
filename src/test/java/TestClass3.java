f31853abab2afe836cbb09fc34ee9c916e21bb82
Invited User iupfvfwp <iupfvfwp@mail.gitlabdemo.cloud>
2022-08-02 13:29:33 +0530

    @DataTableType example

--- src/test/java/TestClass3.java
+++ src/test/java/TestClass3.java
@@ -3,8 +3,12 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import io.qameta.allure.Feature;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.interactions.HasInputDevices;
+import org.openqa.selenium.interactions.Mouse;
 import org.testng.Assert;
 import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
@@ -24,6 +28,16 @@ public class TestClass3 extends BaseTest {
     @Test
     @Severity(SeverityLevel.CRITICAL)
     public void test01_verifyBillPaymentSuccessCase() {
+        /*WebElement element = driver.findElement(By.linkText("Examples"));
+        Actions my_action = new Actions(driver);
+        Thread.sleep(3000);
+        my_action.moveToElement(element).perform();
+        OR
+        WebElement element = driver.findElement(By.linkText("About"));
+        Locatable hoverItem = (Locatable) element;
+        Mouse mouse = ((HasInputDevices) driver).getMouse();
+        mouse.mouseMove(hoverItem.getCoordinates());*/
+
         System.out.println("method 1 Thread ID: " + Thread.currentThread().getId());
         accountServices = loginPage.login("john", "demo");
         billPayPage = accountServices.navigateToBillPay();
