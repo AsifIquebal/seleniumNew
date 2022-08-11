3f635eaded898de8a93d6517216c1fef15d726f6
Invited User iupfvfwp <iupfvfwp@mail.gitlabdemo.cloud>
2022-08-11 13:51:03 +0530

    @DataTableType example

---
+++ src/test/java/TestClass2.java
@@ -0,0 +1,15 @@
+import base.BaseTest;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.BeforeMethod;
+import pageObjects.LoginPage;
+
+public class TestClass2 extends BaseTest {
+    @BeforeMethod
+    public void setUp(){
+
+
+    }
+
+
+}
